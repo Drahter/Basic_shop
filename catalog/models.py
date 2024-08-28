6948c132e80494a624304c1d2289381a3c21e1c5
Dmit Bashmakov <denegniy1@gmail.com>
2024-08-28 17:43:55 +0500

    added data structure for models + pillow module for images

--- catalog/models.py
+++ catalog/models.py
@@ -5,14 +5,21 @@ class Product(models.Model):
     pass
 
     def __str__(self):
-        pass
+        name = models.CharField(max_length=50, verbose_name='наименование')
+        description = models.TextField(max_length=500, verbose_name='описание')
+        image = models.ImageField(verbose_name='изображение')
+        category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+        price = models.IntegerField(max_length=10, verbose_name='цена')
+        created_at = models.DateTimeField(verbose_name='дата создания')
+        updated_at = models.DateTimeField(verbose_name='дата изменения')
 
     class Meta:
         pass
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(max_length=500, verbose_name='описание')
 
     def __str__(self):
         pass
