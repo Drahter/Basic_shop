6d50fb41f55a38f21243dc9e82a57c9229c5505f
Dmit Bashmakov <denegniy1@gmail.com>
2024-08-29 08:06:10 +0500

    added first migrations, corrections to models

--- catalog/models.py
+++ catalog/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 
-class Product(models.Model):
-    pass
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование', null=True)
+    description = models.TextField(max_length=500, verbose_name='описание', null=True)
 
     def __str__(self):
-        name = models.CharField(max_length=50, verbose_name='наименование')
-        description = models.TextField(max_length=500, verbose_name='описание')
-        image = models.ImageField(verbose_name='изображение')
-        category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-        price = models.IntegerField(max_length=10, verbose_name='цена')
-        created_at = models.DateTimeField(verbose_name='дата создания')
-        updated_at = models.DateTimeField(verbose_name='дата изменения')
+        pass
 
     class Meta:
         pass
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name='наименование')
-    description = models.TextField(max_length=500, verbose_name='описание')
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование', null=True)
+    description = models.TextField(max_length=500, verbose_name='описание', null=True)
+    image = models.ImageField(verbose_name='изображение', null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    price = models.IntegerField(verbose_name='цена', null=True)
+    created_at = models.DateTimeField(verbose_name='дата создания', null=True)
+    updated_at = models.DateTimeField(verbose_name='дата изменения', null=True)
 
     def __str__(self):
         pass
