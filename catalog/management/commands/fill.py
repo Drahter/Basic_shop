a998b14f2c8c62f6aadf12bff6dc103b58f15640
Dmit Bashmakov <denegniy1@gmail.com>
2024-09-10 13:56:20 +0500

    changed FILL function for BLOG model

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,7 +1,7 @@
 from django.core.management import BaseCommand
 import json
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, BlogArticle
 
 
 class Command(BaseCommand):
@@ -30,12 +30,26 @@ class Command(BaseCommand):
 
         return products
 
+    @staticmethod
+    def json_read_articles():
+        articles = []
+        with open('catalog/fixtures/blogarticle_data.json', 'r',
+                  encoding='utf8') as file:
+            data = json.load(file)
+            for each in data:
+                if each['model'] == 'catalog.blogarticle':
+                    articles.append(each)
+
+        return articles
+
     def handle(self, *args, **options):
         Category.objects.all().delete()
         Product.objects.all().delete()
+        BlogArticle.objects.all().delete()
 
         product_for_create = []
         category_for_create = []
+        articles_for_create = []
 
         for category in Command.json_read_categories():
             category_for_create.append(
@@ -54,7 +68,24 @@ class Command(BaseCommand):
                         price=product['fields']['price'],
                         created_at=product['fields']['created_at'],
                         updated_at=product['fields']['updated_at']
+
                         )
             )
 
-        Product.objects.bulk_create(product_for_create)
+        Product.objects.bulk_create(articles_for_create)
+
+        for article in Command.json_read_articles():
+            articles_for_create.append(
+                BlogArticle(pk=article['pk'],
+                        title=article['fields']['title'],
+                        content=article['fields']['content'],
+                        created_at=article['fields']['created_at'],
+                        preview_image=article['fields']['preview_image'],
+                        slug=article['fields']['slug'],
+                        is_published=article['fields']['is_published'],
+                        views_counter=article['fields']['views_counter']
+
+                        )
+            )
+
+        BlogArticle.objects.bulk_create(articles_for_create)
