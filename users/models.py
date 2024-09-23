0d524079c92f0185fcee6c2aaa5524af863d6fcf
Drahter <Denegniy1@gmail.com>
2024-09-23 19:43:09 +0500

    added admin settings for users

--- users/models.py
+++ users/models.py
@@ -5,11 +5,14 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Электронная почта')
-    avatar = models.ImageField(upload_to='users/users_avatars/', blank=True, verbose_name='Аватар')
+    avatar = models.ImageField(upload_to='users/users_avatars/', blank=True, null=True, verbose_name='Аватар')
     phone = models.CharField(max_length=35, verbose_name='Телефон')
     country = models.CharField(max_length=35)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
