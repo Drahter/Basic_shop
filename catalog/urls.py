ea3e9e4cc5360fff3aa2ac13fdb3b43529317f2e
Dmit Bashmakov <denegniy1@gmail.com>
2024-09-30 16:00:21 +0500

    added cache for single product page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ContactsView, ProductCreateView, ProductDetailView, ProductUpdateView, \
     ProductDeleteView
@@ -9,7 +11,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('create/', ProductCreateView.as_view(), name='new_product'),
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
